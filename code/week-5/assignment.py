f895f5c5d6f09c4477bca8782b33f9db0aa901e1
jmin0530 <72537757+jmin0530@users.noreply.github.com>
2021-05-08 00:00:26 +0900

    Update assignment.py

--- code/week-5/assignment.py
+++ code/week-5/assignment.py
@@ -72,19 +72,39 @@ def optimum_policy_2D(grid, init, goal, cost):
             if (y, x) == goal and value[(t, y, x)] > 0:
                 # TODO: implement code.
                 value[(t,y,x)] = 0
-                policy[(t,y,x)] = '*'
+                policy[(t,y,x)] = 999
                 change = True
                 
             # Try to use simple arithmetic to capture state transitions.
             elif grid[(y, x)] == 0:
-                # TODO: implement code.
-                for f,a in zip(forward, action_name):
-                    
-                pass
+                # TODO 2:
+                for i in range(len(action)): # action 3가지 경우에 대한 for문 반복
+                    t_next = (t+action[i]) % 4 # 다음에 차량이 향하고자 하는 방향 갱신
+                    y2, x2 = y + forward[t_next][0], x + forward[t_next][1] # action 방향에 따른 다음 위치
+                    if 0 <= y2 < grid.shape[0] and 0 <= x2 < grid.shape[1] and grid[(y2,x2)] == 0:
+                        # 다음 위치가 grid 내에 있고 grid값이 0인 경우에는
+                        # value값 갱신하고 이에따른 policy행렬에 action 방향 저장
+                        v2 = value[(t_next, y2, x2)] + cost[i]
+                        if v2 < value[(t,y,x)]:
+                            change = True
+                            value[(t,y,x)] = v2
+                            policy[(t,y,x)] = action[i]
     # Now navigate through the policy table to generate a
     # sequence of actions to take to follow the optimal path.
-    # TODO: implement code.
+    # TODO 3
+        for i in range(4): # 4가지 방향 중에서 value가 가장 작은 cell로 가는 방향 출력하도록 함.
+            if value[(i,y,x)] < minimum:
+                minimum = value[(i,y,x)]
+                policy2D[(y,x)] = action_name[policy[(theta,y,x)]+1] # cost가 최소가 되도록 현재위치에서의 방향 저장
+                
+        minimum = 999 #  minimum 초기화. 안하면 출력한 방향과 다른 방향으로 경로가 그려진다.
+        
 
+        # 이동 후 theta,y,x값 갱신
+        new_theta = (theta + policy[(theta,y,x)]) % 4
+        y = y + forward[new_theta][0]
+        x = x + forward[new_theta][1]
+        theta=new_theta
     # Return the optimum policy generated above.
     return policy2D
 
